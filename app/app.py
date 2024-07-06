b5612e4f200c5cd9eef308b81dbe01c549f9d4c2
RaulZBergamo <raulbergamo416@gmail.com>
2024-07-06 16:59:49 -0300

    chore: Add docsstrings and __init__.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,12 @@
+"""
+This module contains the main application logic
+
+Endpoints
+
+    / - GET: A health check endpoint
+    /matches/all - GET: An endpoint that returns all matches from HLTV
+"""
+
 import logging
 from flask import Flask
 from services.match_service import MatchService
@@ -8,10 +17,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def init() -> tuple[dict[str, str], int, dict[str, str]]:
+    """
+    An endpoint that returns a health check message
+    """
+
     return ({ "message": "Health check ok" }, 200, { "Content-Type": "application/json" })
 
 @app.route('/matches/all', methods=['GET'])
 def matches() -> tuple[dict[str, str], int, dict[str, str]]:
+    """
+    An endpoint that returns all matches from HLTV
+    """
+
     MatchService().get_matches()
 
 if __name__ == '__main__':
