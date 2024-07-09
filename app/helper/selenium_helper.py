fe52042cede77cd40090339f7d6582064145e649
RaulZBergamo <raulbergamo416@gmail.com>
2024-07-09 12:17:11 -0300

    fix: added headless option to chrome

--- app/helper/selenium_helper.py
+++ app/helper/selenium_helper.py
@@ -14,7 +14,7 @@ class SeleniumHelper:
         logging.info("Initializing WebDriver with Chrome...")
 
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument(f"user-agent={config.USER_AGENT}")
